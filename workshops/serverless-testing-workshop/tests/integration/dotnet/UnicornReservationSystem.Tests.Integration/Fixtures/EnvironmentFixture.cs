5962054d7102e37a8044f9a0c7a9ab1f778be314
Brad Knowles <bradkno@amazon.com>
2024-08-23 11:31:55 -0400

    working tests

--- workshops/serverless-testing-workshop/tests/integration/dotnet/UnicornReservationSystem.Tests.Integration/Fixtures/EnvironmentFixture.cs
+++ workshops/serverless-testing-workshop/tests/integration/dotnet/UnicornReservationSystem.Tests.Integration/Fixtures/EnvironmentFixture.cs
@@ -4,43 +4,46 @@
  * SPDX-License-Identifier: MIT-0
  */
 
-using System;
-using System.Linq;
 using Amazon.CloudFormation;
 using Amazon.CloudFormation.Model;
+
+using System;
+using System.Linq;
 
 namespace UnicornReservationSystem.Tests.Integration.Fixtures;
 
 // https://xunit.net/docs/shared-context#collection-fixture
 public sealed class EnvironmentFixture
 {
-    public EnvironmentFixture()
-    {
-        var stackName = Environment.GetEnvironmentVariable("AWS_SAM_STACK_NAME");
-        if (String.IsNullOrWhiteSpace(stackName))
-        {
-            throw new ApplicationException("Cannot find environment variable AWS_SAM_STACK_NAME. \n" +
-                                           "Please setup this environment variable with the stack name for integration tests");
-        }
+	public EnvironmentFixture()
+	{
+		var stackName = Environment.GetEnvironmentVariable("AWS_SAM_STACK_NAME");
+		if (String.IsNullOrWhiteSpace(stackName))
+		{
+			throw new ApplicationException("Cannot find environment variable AWS_SAM_STACK_NAME. \n" +
+										   "Please setup this environment variable with the stack name for integration tests");
+		}
 
-        try
-        {
-            var client = new AmazonCloudFormationClient();
-            var request = new DescribeStacksRequest
-            {
-                StackName = stackName
-            };
-            var response = client.DescribeStacksAsync(request).GetAwaiter().GetResult();
-            ApiEndpoint = response.Stacks[0].Outputs.First(x => x.OutputKey == "ApiEndpoint").OutputValue;
-            DynamoDbTable = response.Stacks[0].Outputs.First(x => x.OutputKey == "DynamoDBTableName").OutputValue;
-        }
-        catch (Exception e)
-        {
-            throw new ApplicationException($"Cannot find stack {stackName}.\n" +
-                                           "Please make sure stack with the name \"{stackName}\" exists.");
-        }
-    }
-    
-    public string ApiEndpoint { get; private set; }
-    public string DynamoDbTable { get; private set; }
+		try
+		{
+			var client = new AmazonCloudFormationClient();
+			var request = new DescribeStacksRequest
+			{
+				StackName = stackName
+			};
+			var response = client.DescribeStacksAsync(request).GetAwaiter().GetResult();
+			ApiEndpoint = response.Stacks[0].Outputs.First(x => x.OutputKey == "ApiEndpoint").OutputValue;
+			DynamoDbTable = response.Stacks[0].Outputs.First(x => x.OutputKey == "DynamoDBTableName").OutputValue;
+			UnicornInventoryBucket = response.Stacks[0].Outputs.First(x => x.OutputKey == "UnicornInventoryBucket").OutputValue;
+		}
+		catch
+		{
+			throw new ApplicationException($"Cannot find stack {stackName}.\n" +
+										   "Please make sure stack with the name \"{stackName}\" exists.");
+		}
+	}
+
+	public string ApiEndpoint { get; private set; }
+	public string DynamoDbTable { get; private set; }
+	public string UnicornInventoryBucket { get; private set; }
 }
--- workshops/serverless-testing-workshop/tests/integration/dotnet/UnicornReservationSystem.Tests.Integration/LocationTests.cs
+++ workshops/serverless-testing-workshop/tests/integration/dotnet/UnicornReservationSystem.Tests.Integration/LocationTests.cs
@@ -8,7 +8,9 @@ using System.Collections.Generic;
 using System.Net;
 using System.Text.Json;
 using System.Threading.Tasks;
+
 using UnicornReservationSystem.Tests.Integration.Fixtures;
+
 using Xunit;
 
 namespace UnicornReservationSystem.Tests.Integration;
@@ -16,50 +18,50 @@ namespace UnicornReservationSystem.Tests.Integration;
 [Collection("Environment")]
 public class LocationTests : IClassFixture<LocationFixture>
 {
-    private readonly EnvironmentFixture _environmentFixture;
-    private readonly LocationFixture _locationFixture;
+	private readonly EnvironmentFixture _environmentFixture;
+	private readonly LocationFixture _locationFixture;
 
-    public LocationTests(EnvironmentFixture environmentFixture, LocationFixture locationFixture)
-    {
-        _environmentFixture = environmentFixture;
-        _locationFixture = locationFixture;
-    }
-    
-    [Fact]
-    public async Task Api_ConnectivityCheck_Returns200()
-    {
-        // Act
-        var response = await _locationFixture.UnicornApi.GetAsync("locations/");
-        
-        // Assert
-        Assert.True(response.IsSuccessStatusCode);
-        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-    }
+	public LocationTests(EnvironmentFixture environmentFixture, LocationFixture locationFixture)
+	{
+		_environmentFixture = environmentFixture;
+		_locationFixture = locationFixture;
+	}
 
-    [Fact]
-    public async Task Api_GetCorrectLocations_ReturnsLocations()
-    {
-        // Act
-        var response = await _locationFixture.UnicornApi.GetAsync("locations/");
-        var content = await response.Content.ReadAsStringAsync();
-        var result = JsonSerializer.Deserialize<Dictionary<string, string[]>>(content);
+	[Fact]
+	public async Task Api_ConnectivityCheck_Returns200()
+	{
+		// Act
+		var response = await _locationFixture.UnicornApi.GetAsync("locations/");
 
-        // Assert
-        Assert.True(response.IsSuccessStatusCode);
-        Assert.Contains("SOME_LOCATION", result["locations"]);
-    }
-    
-    [Fact]
-    public async Task Api_IncorrectLocations_ReturnsError()
-    {
-        // Act
-        var response = await _locationFixture.UnicornApi.GetAsync("incorrect-locations/");
-        var content = await response.Content.ReadAsStringAsync();
-        var result = JsonSerializer.Deserialize<Dictionary<string, string>>(content);
+		// Assert
+		Assert.True(response.IsSuccessStatusCode);
+		Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+	}
 
-        // Assert
-        Assert.False(response.IsSuccessStatusCode);
-        Assert.Equal(HttpStatusCode.Forbidden, response.StatusCode);
-        Assert.Equal("Missing Authentication Token", result["message"].ToString());
-    }
+	[Fact]
+	public async Task Api_GetCorrectLocations_ReturnsLocations()
+	{
+		// Act
+		var response = await _locationFixture.UnicornApi.GetAsync("locations/");
+		var content = await response.Content.ReadAsStringAsync();
+		var result = JsonSerializer.Deserialize<Dictionary<string, string[]>>(content);
+
+		// Assert
+		Assert.True(response.IsSuccessStatusCode);
+		Assert.Contains("SOME_LOCATION", result["locations"]);
+	}
+
+	[Fact]
+	public async Task Api_IncorrectLocations_ReturnsError()
+	{
+		// Act
+		var response = await _locationFixture.UnicornApi.GetAsync("incorrect-locations/");
+		var content = await response.Content.ReadAsStringAsync();
+		var result = JsonSerializer.Deserialize<Dictionary<string, string>>(content);
+
+		// Assert
+		Assert.False(response.IsSuccessStatusCode);
+		Assert.Equal(HttpStatusCode.Forbidden, response.StatusCode);
+		Assert.Equal("Missing Authentication Token", result["message"].ToString());
+	}
 }
